ea36e74ea7f633da5aa9a05de857df8a09e2367a
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-08 10:00:37 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+class Song 
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  
+  def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre 
+    @@count += 1 
+    @@genres << genre 
+    @@artists << artist
+  end 
+  
+  def count 
+    @@count 
+  end 
+  
+  def artists 
+  
+end 
